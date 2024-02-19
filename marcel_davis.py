7c7915d6ca48bc41717c6a60ae87d9944bcf953a
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2024-02-19 18:10:03 +0100

    bugfixes

--- marcel_davis.py
+++ marcel_davis.py
@@ -10,7 +10,7 @@ import shelve
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# from systemd.journal import JournalHandler
+from systemd.journal import JournalHandler
 
 
 bot = TeleBot(token=API_KEY)
@@ -49,9 +49,8 @@ def parse_week(match) -> list[str]:
 def get_all_menus():
     for download_conf in download_confs:
         weeks = (download_conf.week_key, download_conf.next_week_key)
-        
+        timedelta = 0
         for week in weeks:
-            timedelta = 0
             url = create_URL(download_conf.url, timedelta)
             with requests.get(url, timeout=5) as request:
                 soup = BeautifulSoup(request.content, features="lxml")
@@ -95,26 +94,25 @@ def start(message):
 def hsma_today(message):
     log.info("hsma_today was called")
     menu = menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY][0]
-    # bot.reply_to(message, menu, parse_mode='Markdown')
-    return menu
+    bot.reply_to(message, menu, parse_mode='Markdown')
 
 
 @bot.message_handler(commands=['hsma_this_week'])
 def hsma_this_week(message):
     log.info("mensaweek was called")
     
-    # for day in menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY]:
-    #     bot.reply_to(message, day, parse_mode="Markdown")
+    for day in menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY]:
+        bot.reply_to(message, day, parse_mode="Markdown")
     return [day for day in  menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY]]
 
 @bot.message_handler(commands=['hsma_next_week'])
 def hsma_next_week(message):
     log.info("hsma_next_week was called")
     
-    # for day in menus[DownloadConf.Types.HS][conf.HSMA_MENSA_NEXT_WEEK_KEY]:
-    #     bot.reply_to(message, day, parse_mode="Markdown")
+    for day in menus[DownloadConf.Types.HS][conf.HSMA_MENSA_NEXT_WEEK_KEY]:
+        bot.reply_to(message, day, parse_mode="Markdown")
 
-    return [day for day in  menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY]]
+    # return [day for day in  menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY]]
 
 @bot.message_handler(commands=['uni_today'])
 def uni_today(message):
@@ -146,6 +144,8 @@ def abo(message):
     chatid = int(message.chat.id)
     abos : list[int] = []
     with shelve.open(conf.SHELVE_FILE_NAME) as file:
+        if conf.ABO_KEY not in file:
+            file[conf.ABO_KEY] = []
         abos = file[conf.ABO_KEY]
         if chatid not in abos:
             abos.append(chatid)
@@ -169,10 +169,10 @@ def send_all_abos():
     with shelve.open(conf.SHELVE_FILE_NAME) as file:
         abos = file[conf.ABO_KEY]
     log.info(f"sending abos. currently there are {len(abos)} abos")
-    menu = ""
+    menu = menus[DownloadConf.Types.HS][conf.HSMA_MENSA_THIS_WEEK_KEY][0]
     if len(abos) > 0:
         for chat_id in abos:
-            bot.send_message(chat_id, menu)
+            bot.send_message(chat_id, menu, parse_mode="Markdown")
 
 
 def bot_poll():
@@ -194,9 +194,13 @@ def run_scheduler():
         year="*",
         month="*",
         day_of_week="0-4",
+        # day="*",
         hour=4,
+        # hour="*",
         minute=0,
+        # minute="*",
         second=0
+        # second="*/5"
     )
     sched.add_job(
         send_all_abos,
@@ -204,9 +208,13 @@ def run_scheduler():
         year="*",
         month="*",
         day_of_week="0-4",
-        hour=7,
+        # day="*",
+        hour=7
+        # hour="*",
         minute=0,
+        # minute="*",
         second=0
+        # second="*/5"
     )
     sched.start()
 
@@ -255,8 +263,8 @@ def main():
 
 if __name__ == '__main__':
     log = logging.getLogger("marcel_davis")
-    # log.addHandler(JournalHandler())
-    log.setLevel(logging.INFO)
+    log.addHandler(JournalHandler())
+    # log.setLevel(logging.INFO)
     logging.basicConfig(level=logging.INFO)
     log.info("starting bot")
     startup(download_confs=download_confs)
